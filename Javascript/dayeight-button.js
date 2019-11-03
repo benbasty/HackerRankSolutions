f344548afa73023964a9c676e13715630b00b5f2
Ben Basty <benbasty@yahoo.fr>
2019-11-03 22:05:27 +0800

    javascript coding challenges

---
+++ Javascript/dayeight-button.js
@@ -0,0 +1,24 @@
+// CSS:
+// #btn{
+//     width: 96px;
+//     height: 48px;
+//     font-size: 24px;
+// }
+// JavaScript:
+var btn = document.getElementById("btn");
+btn.addEventListener("click",function() {btn.innerHTML++});
+// HTML:
+// <!-- Enter your HTML code here -->
+// <!DOCTYPE html>
+// <html>
+//     <head>
+//         <meta charset="utf-8">
+//         <title>Button</title>
+//         <link rel="stylesheet" href="css/button.css" type="text/css">
+//     </head>  
+//     <body>
+//         <button id="btn">0</button>
+//         <script src="js/button.js" type="text/javascript"></script>
+//     </body>
+    
+// </html>
